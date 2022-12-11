6f8b1743438bdeb8282dc0cb4af3a455aa66de5e
Sivaranjani-A <ranjani731997@gmail.com>
2022-12-11 12:35:43 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
-import logo from './logo.svg';
 import './App.css';
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import { useState } from 'react';
+import Login from './Login/Login';
+import { Route, Routes } from 'react-router-dom';
+import UserList from './Users/UserList';
+import Portal from './Portal/Portal';
+import CreateUser from './Users/CreateUser';
+import UserView from './Users/UserView';
+import EditUser from './Users/EditUser';
+
 
 function App() {
+  const [data, setData] = useState([
+    {
+      id: 5002,
+      name: "ranjan",
+      email: "ranjan@gmail.com",
+      role: "fullsatckdeveloper",
+      salary: "30000"
+    },
+    {
+      id: 5003,
+      name: "sivaranjani",
+      email: "ranjani731997@gmail.com",
+      role: "software Engineer",
+      salary: "50000"
+    }
+  ])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path='/' element={<Login />} />
+      <Route path="/portal" element={<Portal />}>
+        <Route path="userlist" element={<UserList data={data} setData={setData} />} />
+        <Route path="createuser" element={<CreateUser data={data} setData={setData} />} />
+        <Route path="userview/:id" element={<UserView data={data} />} />
+        <Route path="edituser/:id" element={<EditUser data={data} />} />
+      </Route>
+    </Routes>
   );
 }
 
